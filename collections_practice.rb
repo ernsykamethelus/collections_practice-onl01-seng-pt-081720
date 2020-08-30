78719e539d9fa97018a40bc2b5def1206482a8e9
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-08-30 20:39:41 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,36 @@
+num= ["25", "7", "1"]
 
+def sort_array_asc(num)
+  num.sort do |x,y|
+  x<=>y
+end
+end
+
+
+num= ["25", "7", "1"]
+def sort_array_desc(num)
+  num.sort do |a,b|
+    b<=>a
+  end
+end
+
+name= ["dogs", "cat", "Horses"]
+
+def sort_array_char_count(name)
+  name.sort!
+  end
+
+names= ["blake", "ashley", "scott"]
+
+def swap_elements(names)
+  names.sort do |a,b|
+    b<=>a
+  end
+end
+
+
+num= ["12", "4", "35"]
+
+def reverse_array(num)
+  num.reverse
+end  
